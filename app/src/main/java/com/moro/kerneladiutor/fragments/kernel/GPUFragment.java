251bef035c8ca20ef5793e6bc7f0f63414208b36
morogoku <morogoku@hotmail.com>
2017-04-05 23:49:11 +0200

    GPU Volt

--- app/src/main/java/com/moro/kerneladiutor/fragments/kernel/GPUFragment.java
+++ app/src/main/java/com/moro/kerneladiutor/fragments/kernel/GPUFragment.java
@@ -22,6 +22,7 @@ package com.moro.kerneladiutor.fragments.kernel;
 import com.moro.kerneladiutor.R;
 import com.moro.kerneladiutor.fragments.ApplyOnBootFragment;
 import com.moro.kerneladiutor.fragments.RecyclerViewFragment;
+import com.moro.kerneladiutor.utils.Utils;
 import com.moro.kerneladiutor.utils.kernel.gpu.GPUFreq;
 import com.moro.kerneladiutor.views.recyclerview.CardView;
 import com.moro.kerneladiutor.views.recyclerview.RecyclerViewItem;
@@ -33,6 +34,7 @@ import com.moro.kerneladiutor.views.recyclerview.XYGraphView;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Created by willi on 12.05.16.
@@ -51,8 +53,12 @@ public class GPUFragment extends RecyclerViewFragment {
     @Override
     protected void addItems(List<RecyclerViewItem> items) {
         freqInit(items);
-        governorInit(items);
-
+        if (GPUFreq.hasGovernor()){
+            governorInit(items);
+        }
+        if (GPUFreq.hasBackup()){
+            voltageInit(items);
+        }
     }
 
     private void freqInit(List<RecyclerViewItem> items) {
@@ -128,8 +134,7 @@ public class GPUFragment extends RecyclerViewFragment {
 
         if (GPUFreq.hasHighspeedClock()){
             List<String> freqs = new ArrayList<>();
-            List<Integer> list = GPUFreq.getAvailableS7Freqs();
-            Collections.sort(list);
+            List<Integer> list = GPUFreq.getAvailableS7FreqsSort();
             int value = 0;
             for (int i = 0; i < list.size(); i++) {
                 freqs.add(String.valueOf(list.get(i)));
@@ -206,6 +211,73 @@ public class GPUFragment extends RecyclerViewFragment {
         }
     }
 
+    private void voltageInit(List<RecyclerViewItem> items) {
+        CardView voltCard = new CardView(getActivity());
+        voltCard.setTitle(getString(R.string.gpu_voltage));
+
+        List<Integer> freqs = GPUFreq.getAvailableS7Freqs();
+        List<String> voltages = GPUFreq.getVoltages();
+        List<String> voltagesStock = GPUFreq.getStockVoltages();
+
+        if (freqs != null && voltages != null && freqs.size() == voltages.size()) {
+            for (int i = 0; i < freqs.size(); i++) {
+                SeekBarView seekbar = new SeekBarView();
+                seekbarInit(seekbar, freqs.get(i), voltages.get(i), voltagesStock.get(i));
+                voltCard.addItem(seekbar);
+            }
+        }
+        
+        if (voltCard.size() > 0) {
+            items.add(voltCard);
+        }
+    }
+
+    private void seekbarInit(SeekBarView seekbar, final Integer freq, String voltage,
+                             String voltageStock) {
+
+        int mStep = 6250;
+        int mOffset = GPUFreq.getOffset();
+        float mMin = (Utils.strToFloat(voltageStock) - 100) * mOffset;
+        float mMax = ((Utils.strToFloat(voltageStock) + 25) * mOffset) + mStep;
+
+        List<String> progress = new ArrayList<>();
+        for(float i = mMin ; i < mMax; i += mStep){
+            String string = String.valueOf(i / mOffset);
+            progress.add(string);
+        }
+
+        int value = 0;
+        for (int i = 0; i < progress.size(); i++) {
+            if (Objects.equals(progress.get(i), voltage)){
+                value = i;
+                break;
+            }
+        }
+
+        seekbar.setTitle(freq + " " + getString(R.string.mhz));
+        seekbar.setSummary(getString(R.string.def) + ": " + voltageStock + " " + getString(R.string.mv));
+        seekbar.setUnit(getString(R.string.mv));
+        seekbar.setItems(progress);
+        seekbar.setProgress(value);
+        seekbar.setOnSeekBarListener(new SeekBarView.OnSeekBarListener() {
+
+            @Override
+            public void onStop(SeekBarView seekBarView, int position, String value) {
+                GPUFreq.setVoltage(freq, value, getActivity());
+                /*getHandler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        reload();
+                    }
+                }, 200);*/
+            }
+
+            @Override
+            public void onMove(SeekBarView seekBarView, int position, String value) {
+            }
+        });
+    }
+
     @Override
     protected void refresh() {
         super.refresh();
@@ -215,11 +287,12 @@ public class GPUFragment extends RecyclerViewFragment {
             String text = "";
 
             int freq = GPUFreq.getCurFreq();
-            float maxFreq = GPUFreq.getAvailableS7Freqs().get(GPUFreq.getAvailableS7Freqs().size() - 1);
+            float maxFreq = GPUFreq.getAvailableS7FreqsSort().get(GPUFreq.getAvailableS7FreqsSort().size() - 1);
             text += freq / GPUFreq.getCurFreqOffset() + getString(R.string.mhz);
             mCurFreq.setText(text);
             float per = (float) freq / maxFreq * 100f;
             mCurFreq.addPercentage(load >= 0 ? load : Math.round(per > 100 ? 100 : per < 0 ? 0 : per));
         }
+
     }
 }
--- app/src/main/java/com/moro/kerneladiutor/utils/kernel/gpu/GPUFreq.java
+++ app/src/main/java/com/moro/kerneladiutor/utils/kernel/gpu/GPUFreq.java
@@ -36,6 +36,8 @@ import java.util.List;
  */
 public class GPUFreq {
 
+    private static final String BACKUP = "/data/.moro/bk/gpu_stock_voltage";
+
     private static final String MAX_S7_FREQ = "/sys/devices/14ac0000.mali/max_clock";
     private static final String MIN_S7_FREQ = "/sys/devices/14ac0000.mali/min_clock";
     private static final String CUR_S7_FREQ = "/sys/devices/14ac0000.mali/clock";
@@ -70,6 +72,7 @@ public class GPUFreq {
     private static String CUR_FREQ;
     private static Integer CUR_FREQ_OFFSET;
     private static List<Integer> AVAILABLE_FREQS;
+    private static List<Integer> AVAILABLE_FREQS_SORT;
     private static String MAX_FREQ;
     private static Integer MAX_FREQ_OFFSET;
     private static String MIN_FREQ;
@@ -77,6 +80,10 @@ public class GPUFreq {
     private static String GOVERNOR;
     private static String[] AVAILABLE_GOVERNORS;
     private static Integer AVAILABLE_GOVERNORS_OFFSET;
+
+    private static String SPLIT_NEW_LINE = "\\r?\\n";
+    private static String SPLIT_LINE = " ";
+    private static Integer VOLT_OFFSET = 1000;
 
     public static void setS7Governor(String value, Context context) {
         switch (value){
@@ -212,8 +219,27 @@ public class GPUFreq {
             }
         }
         if (AVAILABLE_FREQS == null) return null;
-        Collections.sort(AVAILABLE_FREQS);
         return AVAILABLE_FREQS;
+    }
+
+    public static List<Integer> getAvailableS7FreqsSort() {
+        if (AVAILABLE_FREQS_SORT == null) {
+            for (String file : sAvailableFreqs.keySet()) {
+                if (Utils.existFile(file)) {
+                    String freqs[] = Utils.readFile(file).split("\\r?\\n");
+                    AVAILABLE_FREQS_SORT = new ArrayList<>();
+                    for (String freq : freqs) {
+                        String[] freqLine = freq.split(" ");
+                        AVAILABLE_FREQS_SORT.add(Utils.strToInt(freqLine[0].trim()));
+                    }
+                    AVAILABLE_GOVERNORS_OFFSET = sAvailableFreqs.get(file);
+                    break;
+                }
+            }
+        }
+        if (AVAILABLE_FREQS_SORT == null) return null;
+        Collections.sort(AVAILABLE_FREQS_SORT);
+        return AVAILABLE_FREQS_SORT;
     }
 
     public static int getCurFreqOffset() {
@@ -271,6 +297,55 @@ public class GPUFreq {
 
     public static boolean hasHighspeedDelay() {
         return Utils.existFile(TUNABLE_HIGHSPEED_DELAY);
+    }
+
+    public static void setVoltage(Integer freq, String voltage, Context context) {
+
+        //freq = String.valueOf(freq);
+        String volt = String.valueOf((int)(Utils.strToFloat(voltage) * VOLT_OFFSET));
+        run(Control.write(freq + " " + volt, AVAILABLE_S7_FREQS), AVAILABLE_S7_FREQS + freq, context);
+    }
+
+    public static List<String> getStockVoltages() {
+        String value = Utils.readFile(BACKUP);
+        if (!value.isEmpty()) {
+            String[] lines = value.split(SPLIT_NEW_LINE);
+            List<String> voltages = new ArrayList<>();
+            for (String line : lines) {
+                String[] voltageLine = line.split(SPLIT_LINE);
+                if (voltageLine.length > 1) {
+                    voltages.add(String.valueOf(Utils.strToFloat(voltageLine[1].trim()) / VOLT_OFFSET));
+
+                }
+            }
+            return voltages;
+        }
+        return null;
+    }
+
+    public static List<String> getVoltages() {
+        String value = Utils.readFile(AVAILABLE_S7_FREQS);
+        if (!value.isEmpty()) {
+            String[] lines = value.split(SPLIT_NEW_LINE);
+            List<String> voltages = new ArrayList<>();
+            for (String line : lines) {
+                String[] voltageLine = line.split(SPLIT_LINE);
+                if (voltageLine.length > 1) {
+                    voltages.add(String.valueOf(Utils.strToFloat(voltageLine[1].trim()) / VOLT_OFFSET));
+
+                }
+            }
+            return voltages;
+        }
+        return null;
+    }
+
+    public static int getOffset () {
+        return VOLT_OFFSET;
+    }
+
+    public static boolean hasBackup() {
+        return Utils.existFile(BACKUP);
     }
 
     public static boolean supported() {
