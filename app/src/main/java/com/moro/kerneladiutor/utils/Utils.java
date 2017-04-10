6138f88d957195905c7db8734c6d421ed025a972
Morogoku <morogoku@hotmail.com>
2017-04-10 16:21:23 +0200

    Export profile folder to Mtweaks2

--- app/src/main/java/com/moro/kerneladiutor/utils/Utils.java
+++ app/src/main/java/com/moro/kerneladiutor/utils/Utils.java
@@ -222,8 +222,7 @@ public class Utils {
     }
 
     public static String getInternalDataStorage() {
-        return Environment.getExternalStorageDirectory().toString() + "/Android/data/" +
-                BuildConfig.APPLICATION_ID;
+        return Environment.getExternalStorageDirectory().toString() + "/Mtweaks2";
     }
 
     // Sorry pirates!
