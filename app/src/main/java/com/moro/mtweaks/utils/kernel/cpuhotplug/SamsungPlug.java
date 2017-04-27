752e5fb901a92e84bab7a4f77ce89e98bba30953
morogoku <morogoku@hotmail.com>
2017-04-27 03:24:50 +0200

    CPUHotplug: add Samsung Hotplug #3

--- app/src/main/java/com/moro/mtweaks/utils/kernel/cpuhotplug/SamsungPlug.java
+++ app/src/main/java/com/moro/mtweaks/utils/kernel/cpuhotplug/SamsungPlug.java
@@ -12,27 +12,45 @@ import com.moro.mtweaks.utils.root.Control;
 
 public class SamsungPlug {
 
-    private static final String HOTPLUG_SAMSUNG_PLUG = "/sys/power/cpuhotplug";
-    private static final String HOTPLUG_SAMSUNG_PLUG_ENABLE = HOTPLUG_SAMSUNG_PLUG + "/enabled";
-    private static final String HOTPLUG_SAMSUNG_PLUG_MAX_ONLINE_CPU = HOTPLUG_SAMSUNG_PLUG + "/max_online_cpu";
-    private static final String HOTPLUG_SAMSUNG_PLUG_MIN_ONLINE_CPU = HOTPLUG_SAMSUNG_PLUG + "/min_online_cpu";
-
-
+    private static final String HOTPLUG_SAMSUNG = "/sys/power/cpuhotplug";
+    private static final String HOTPLUG_SAMSUNG_ENABLE = HOTPLUG_SAMSUNG + "/enabled";
+    private static final String HOTPLUG_SAMSUNG_MAX_ONLINE_CPU = HOTPLUG_SAMSUNG + "/max_online_cpu";
+    private static final String HOTPLUG_SAMSUNG_MIN_ONLINE_CPU = HOTPLUG_SAMSUNG + "/min_online_cpu";
 
     public static void enableSamsungPlug(boolean enable, Context context) {
-        run(Control.write(enable ? "1" : "0", HOTPLUG_SAMSUNG_PLUG_ENABLE), HOTPLUG_SAMSUNG_PLUG_ENABLE, context);
+        run(Control.write(enable ? "1" : "0", HOTPLUG_SAMSUNG_ENABLE), HOTPLUG_SAMSUNG_ENABLE, context);
     }
 
     public static boolean isSamsungPlugEnabled() {
-        return Utils.readFile(HOTPLUG_SAMSUNG_PLUG_ENABLE).equals("1");
+        return Utils.readFile(HOTPLUG_SAMSUNG_ENABLE).equals("1");
     }
 
-    public static boolean hasSamsungPlugEnable() {
-        return Utils.existFile(HOTPLUG_SAMSUNG_PLUG_ENABLE);
+    public static String getMaxOnlineCpu() {
+        String value = Utils.readFile(HOTPLUG_SAMSUNG_MAX_ONLINE_CPU);
+        if (!value.isEmpty()) {
+            return value.replace("max online cpu : ", "");
+        }
+        return null;
+    }
+
+    public static void setMaxOnlineCpu(int value, Context context) {
+        run(Control.write(String.valueOf(value), HOTPLUG_SAMSUNG_MAX_ONLINE_CPU), HOTPLUG_SAMSUNG_MAX_ONLINE_CPU, context);
+    }
+
+    public static String getMinOnlineCpu() {
+        String value = Utils.readFile(HOTPLUG_SAMSUNG_MIN_ONLINE_CPU);
+        if (!value.isEmpty()) {
+            return value.replace("min online cpu : ", "");
+        }
+        return null;
+    }
+
+    public static void setMinOnlineCpu(int value, Context context) {
+        run(Control.write(String.valueOf(value), HOTPLUG_SAMSUNG_MIN_ONLINE_CPU), HOTPLUG_SAMSUNG_MIN_ONLINE_CPU, context);
     }
 
     public static boolean supported() {
-        return Utils.existFile(HOTPLUG_SAMSUNG_PLUG);
+        return Utils.existFile(HOTPLUG_SAMSUNG);
     }
 
     private static void run(String command, String id, Context context) {
