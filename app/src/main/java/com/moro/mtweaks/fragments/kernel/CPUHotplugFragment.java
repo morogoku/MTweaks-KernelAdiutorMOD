SmartPack/SmartPack-Kernel-Manager

CPUHotplug: Add AiOHotplug

app/src/main/java/com/grarak/kerneladiutor/fragments/kernel/CPUHotplugFragment.java
@@ -24,6 +24,7 @@ import com.grarak.kerneladiutor.fragments.ApplyOnBootFragment;
 import com.grarak.kerneladiutor.fragments.RecyclerViewFragment;
 import com.grarak.kerneladiutor.utils.Utils;
 import com.grarak.kerneladiutor.utils.kernel.cpu.CPUFreq;
+import com.grarak.kerneladiutor.utils.kernel.cpuhotplug.AiOHotplug;
 import com.grarak.kerneladiutor.utils.kernel.cpuhotplug.AlucardHotplug;
 import com.grarak.kerneladiutor.utils.kernel.cpuhotplug.AutoSmp;
 import com.grarak.kerneladiutor.utils.kernel.cpuhotplug.BluPlug;
@@ -100,6 +101,9 @@ public class CPUHotplugFragment extends RecyclerViewFragment {
         }
         if (CoreCtl.supported()) {
             coreCtlInit(items);
+        }
+        if (AiOHotplug.supported()) {
+            aioHotplugInit(items);
         }
 
         for (SwitchView view : mEnableViews) {
@@ -2288,6 +2292,106 @@ public class CPUHotplugFragment extends RecyclerViewFragment {
             }
 
             items.addAll(coreCtl);
+        }
+    }
+
+    private void aioHotplugInit(List<RecyclerViewItem> items) {
+        List<RecyclerViewItem> aioHotplug = new ArrayList<>();
+        TitleView title = new TitleView();
+        title.setText(getString(R.string.aio_hotplug));
+
+        if (AiOHotplug.hasToggle()) {
+            SwitchView toggle = new SwitchView();
+            toggle.setTitle(getString(R.string.aio_hotplug));
+            toggle.setSummary(getString(R.string.aio_hotplug_summary));
+            toggle.setChecked(AiOHotplug.isEnabled());
+            toggle.addOnSwitchListener(new SwitchView.OnSwitchListener() {
+                @Override
+                public void onChanged(SwitchView switchView, boolean isChecked) {
+                    AiOHotplug.enable(isChecked, getActivity());
+                }
+            });
+
+            aioHotplug.add(toggle);
+            mEnableViews.add(toggle);
+        }
+
+        if (AiOHotplug.hasCores()) {
+            SeekBarView maxCpus = new SeekBarView();
+            maxCpus.setTitle(getString(R.string.max_cpu_online));
+            maxCpus.setSummary(getString(R.string.max_cpu_online_summary));
+            maxCpus.setMax(CPUFreq.getCpuCount());
+            maxCpus.setMin(1);
+            maxCpus.setProgress(AiOHotplug.getCores() - 1);
+            maxCpus.setOnSeekBarListener(new SeekBarView.OnSeekBarListener() {
+                @Override
+                public void onMove(SeekBarView seekBarView, int position, String value) {
+                }
+
+                @Override
+                public void onStop(SeekBarView seekBarView, int position, String value) {
+                    AiOHotplug.setCores(position + 1, getActivity());
+                }
+            });
+
+            aioHotplug.add(maxCpus);
+        }
+
+        if (CPUFreq.isBigLITTLE() && AiOHotplug.hasBigCores()) {
+            List<String> list = new ArrayList<>();
+            list.add("Disable");
+            for (int i = 1; i <= CPUFreq.getBigCpuRange().size(); i++) {
+                list.add(String.valueOf(i));
+            }
+
+            SeekBarView bigMaxCpus = new SeekBarView();
+            bigMaxCpus.setTitle(getString(R.string.max_cpu_online_big));
+            bigMaxCpus.setSummary(getString(R.string.max_cpu_online_big_summary));
+            bigMaxCpus.setItems(list);
+            bigMaxCpus.setProgress(AiOHotplug.getBigCores());
+            bigMaxCpus.setOnSeekBarListener(new SeekBarView.OnSeekBarListener() {
+                @Override
+                public void onMove(SeekBarView seekBarView, int position, String value) {
+                }
+
+                @Override
+                public void onStop(SeekBarView seekBarView, int position, String value) {
+                    AiOHotplug.setBigCores(position, getActivity());
+                }
+            });
+
+            aioHotplug.add(bigMaxCpus);
+        }
+
+        if (CPUFreq.isBigLITTLE() && AiOHotplug.hasLITTLECores()) {
+            List<String> list = new ArrayList<>();
+            list.add("Disable");
+            for (int i = 1; i <= CPUFreq.getLITTLECpuRange().size(); i++) {
+                list.add(String.valueOf(i));
+            }
+
+            SeekBarView LITTLEMaxCpus = new SeekBarView();
+            LITTLEMaxCpus.setTitle(getString(R.string.max_cpu_online_little));
+            LITTLEMaxCpus.setSummary(getString(R.string.max_cpu_online_little_summary));
+            LITTLEMaxCpus.setItems(list);
+            LITTLEMaxCpus.setProgress(AiOHotplug.getLITTLECores());
+            LITTLEMaxCpus.setOnSeekBarListener(new SeekBarView.OnSeekBarListener() {
+                @Override
+                public void onMove(SeekBarView seekBarView, int position, String value) {
+                }
+
+                @Override
+                public void onStop(SeekBarView seekBarView, int position, String value) {
+                    AiOHotplug.setLITTLECores(position, getActivity());
+                }
+            });
+
+            aioHotplug.add(LITTLEMaxCpus);
+        }
+
+        if (aioHotplug.size() > 0) {
+            items.add(title);
+            items.addAll(aioHotplug);
         }
     }
 

app/src/main/java/com/grarak/kerneladiutor/utils/kernel/cpuhotplug/AiOHotplug.java
@@ -1,0 +1,77 @@
+package com.grarak.kerneladiutor.utils.kernel.cpuhotplug;
+
+import android.content.Context;
+
+import com.grarak.kerneladiutor.fragments.ApplyOnBootFragment;
+import com.grarak.kerneladiutor.utils.Utils;
+import com.grarak.kerneladiutor.utils.root.Control;
+
+/**
+ * Created by willi on 19.06.17.
+ */
+
+public class AiOHotplug {
+
+    private static final String PARENT = "/sys/kernel/AiO_HotPlug";
+    private static final String TOGGLE = PARENT + "/toggle";
+    private static final String CORES = PARENT + "/cores";
+    private static final String BIG_CORES = PARENT + "/big_cores";
+    private static final String LITTLE_CORES = PARENT + "/LITTLE_cores";
+
+    public static void setLITTLECores(int cores, Context context) {
+        run(Control.write(String.valueOf(cores), LITTLE_CORES), LITTLE_CORES, context);
+    }
+
+    public static int getLITTLECores() {
+        return Utils.strToInt(Utils.readFile(LITTLE_CORES));
+    }
+
+    public static boolean hasLITTLECores() {
+        return Utils.existFile(LITTLE_CORES);
+    }
+
+    public static void setBigCores(int cores, Context context) {
+        run(Control.write(String.valueOf(cores), BIG_CORES), BIG_CORES, context);
+    }
+
+    public static int getBigCores() {
+        return Utils.strToInt(Utils.readFile(BIG_CORES));
+    }
+
+    public static boolean hasBigCores() {
+        return Utils.existFile(BIG_CORES);
+    }
+
+    public static void setCores(int cores, Context context) {
+        run(Control.write(String.valueOf(cores), CORES), CORES, context);
+    }
+
+    public static int getCores() {
+        return Utils.strToInt(Utils.readFile(CORES));
+    }
+
+    public static boolean hasCores() {
+        return Utils.existFile(CORES);
+    }
+
+    public static void enable(boolean enable, Context context) {
+        run(Control.write(enable ? "1" : "0", TOGGLE), TOGGLE, context);
+    }
+
+    public static boolean isEnabled() {
+        return Utils.readFile(TOGGLE).equals("1");
+    }
+
+    public static boolean hasToggle() {
+        return Utils.existFile(TOGGLE);
+    }
+
+    public static boolean supported() {
+        return Utils.existFile(PARENT);
+    }
+
+    private static void run(String command, String id, Context context) {
+        Control.runSetting(command, ApplyOnBootFragment.CPU_HOTPLUG, id, context);
+    }
+
+}

morogoku/MTweaks-KernelAdiutorMOD

app/src/main/java/com/moro/mtweaks/fragments/kernel/CPUHotplugFragment.java
@@ -24,6 +24,7 @@ import com.moro.mtweaks.fragments.ApplyOnBootFragment;
 import com.moro.mtweaks.fragments.RecyclerViewFragment;
 import com.moro.mtweaks.utils.Utils;
 import com.moro.mtweaks.utils.kernel.cpu.CPUFreq;
+import com.moro.mtweaks.utils.kernel.cpuhotplug.AiOHotplug;
 import com.moro.mtweaks.utils.kernel.cpuhotplug.AlucardHotplug;
 import com.moro.mtweaks.utils.kernel.cpuhotplug.AutoSmp;
 import com.moro.mtweaks.utils.kernel.cpuhotplug.BluPlug;
@@ -105,6 +106,9 @@ public class CPUHotplugFragment extends RecyclerViewFragment {
         }
         if (CoreCtl.supported()) {
             coreCtlInit(items);
+        }
+        if (AiOHotplug.supported()) {
+            aioHotplugInit(items);
         }
 /*
         for (SwitchView view : mEnableViews) {
@@ -2385,6 +2389,106 @@ public class CPUHotplugFragment extends RecyclerViewFragment {
             }
 
             items.addAll(coreCtl);
+        }
+    }
+
+    private void aioHotplugInit(List<RecyclerViewItem> items) {
+        List<RecyclerViewItem> aioHotplug = new ArrayList<>();
+        TitleView title = new TitleView();
+        title.setText(getString(R.string.aio_hotplug));
+
+        if (AiOHotplug.hasToggle()) {
+            SwitchView toggle = new SwitchView();
+            toggle.setTitle(getString(R.string.aio_hotplug));
+            toggle.setSummary(getString(R.string.aio_hotplug_summary));
+            toggle.setChecked(AiOHotplug.isEnabled());
+            toggle.addOnSwitchListener(new SwitchView.OnSwitchListener() {
+                @Override
+                public void onChanged(SwitchView switchView, boolean isChecked) {
+                    AiOHotplug.enable(isChecked, getActivity());
+                }
+            });
+
+            aioHotplug.add(toggle);
+            mEnableViews.add(toggle);
+        }
+
+        if (AiOHotplug.hasCores()) {
+            SeekBarView maxCpus = new SeekBarView();
+            maxCpus.setTitle(getString(R.string.max_cpu_online));
+            maxCpus.setSummary(getString(R.string.max_cpu_online_summary));
+            maxCpus.setMax(CPUFreq.getCpuCount());
+            maxCpus.setMin(1);
+            maxCpus.setProgress(AiOHotplug.getCores() - 1);
+            maxCpus.setOnSeekBarListener(new SeekBarView.OnSeekBarListener() {
+                @Override
+                public void onMove(SeekBarView seekBarView, int position, String value) {
+                }
+
+                @Override
+                public void onStop(SeekBarView seekBarView, int position, String value) {
+                    AiOHotplug.setCores(position + 1, getActivity());
+                }
+            });
+
+            aioHotplug.add(maxCpus);
+        }
+
+        if (CPUFreq.isBigLITTLE() && AiOHotplug.hasBigCores()) {
+            List<String> list = new ArrayList<>();
+            list.add("Disable");
+            for (int i = 1; i <= CPUFreq.getBigCpuRange().size(); i++) {
+                list.add(String.valueOf(i));
+            }
+
+            SeekBarView bigMaxCpus = new SeekBarView();
+            bigMaxCpus.setTitle(getString(R.string.max_cpu_online_big));
+            bigMaxCpus.setSummary(getString(R.string.max_cpu_online_big_summary));
+            bigMaxCpus.setItems(list);
+            bigMaxCpus.setProgress(AiOHotplug.getBigCores());
+            bigMaxCpus.setOnSeekBarListener(new SeekBarView.OnSeekBarListener() {
+                @Override
+                public void onMove(SeekBarView seekBarView, int position, String value) {
+                }
+
+                @Override
+                public void onStop(SeekBarView seekBarView, int position, String value) {
+                    AiOHotplug.setBigCores(position, getActivity());
+                }
+            });
+
+            aioHotplug.add(bigMaxCpus);
+        }
+
+        if (CPUFreq.isBigLITTLE() && AiOHotplug.hasLITTLECores()) {
+            List<String> list = new ArrayList<>();
+            list.add("Disable");
+            for (int i = 1; i <= CPUFreq.getLITTLECpuRange().size(); i++) {
+                list.add(String.valueOf(i));
+            }
+
+            SeekBarView LITTLEMaxCpus = new SeekBarView();
+            LITTLEMaxCpus.setTitle(getString(R.string.max_cpu_online_little));
+            LITTLEMaxCpus.setSummary(getString(R.string.max_cpu_online_little_summary));
+            LITTLEMaxCpus.setItems(list);
+            LITTLEMaxCpus.setProgress(AiOHotplug.getLITTLECores());
+            LITTLEMaxCpus.setOnSeekBarListener(new SeekBarView.OnSeekBarListener() {
+                @Override
+                public void onMove(SeekBarView seekBarView, int position, String value) {
+                }
+
+                @Override
+                public void onStop(SeekBarView seekBarView, int position, String value) {
+                    AiOHotplug.setLITTLECores(position, getActivity());
+                }
+            });
+
+            aioHotplug.add(LITTLEMaxCpus);
+        }
+
+        if (aioHotplug.size() > 0) {
+            items.add(title);
+            items.addAll(aioHotplug);
         }
     }
 

app/src/main/java/com/moro/mtweaks/utils/kernel/cpuhotplug/AiOHotplug.java
@@ -1,0 +1,77 @@
+package com.moro.mtweaks.utils.kernel.cpuhotplug;
+
+import android.content.Context;
+
+import com.moro.mtweaks.fragments.ApplyOnBootFragment;
+import com.moro.mtweaks.utils.Utils;
+import com.moro.mtweaks.utils.root.Control;
+
+/**
+ * Created by willi on 19.06.17.
+ */
+
+public class AiOHotplug {
+
+    private static final String PARENT = "/sys/kernel/AiO_HotPlug";
+    private static final String TOGGLE = PARENT + "/toggle";
+    private static final String CORES = PARENT + "/cores";
+    private static final String BIG_CORES = PARENT + "/big_cores";
+    private static final String LITTLE_CORES = PARENT + "/LITTLE_cores";
+
+    public static void setLITTLECores(int cores, Context context) {
+        run(Control.write(String.valueOf(cores), LITTLE_CORES), LITTLE_CORES, context);
+    }
+
+    public static int getLITTLECores() {
+        return Utils.strToInt(Utils.readFile(LITTLE_CORES));
+    }
+
+    public static boolean hasLITTLECores() {
+        return Utils.existFile(LITTLE_CORES);
+    }
+
+    public static void setBigCores(int cores, Context context) {
+        run(Control.write(String.valueOf(cores), BIG_CORES), BIG_CORES, context);
+    }
+
+    public static int getBigCores() {
+        return Utils.strToInt(Utils.readFile(BIG_CORES));
+    }
+
+    public static boolean hasBigCores() {
+        return Utils.existFile(BIG_CORES);
+    }
+
+    public static void setCores(int cores, Context context) {
+        run(Control.write(String.valueOf(cores), CORES), CORES, context);
+    }
+
+    public static int getCores() {
+        return Utils.strToInt(Utils.readFile(CORES));
+    }
+
+    public static boolean hasCores() {
+        return Utils.existFile(CORES);
+    }
+
+    public static void enable(boolean enable, Context context) {
+        run(Control.write(enable ? "1" : "0", TOGGLE), TOGGLE, context);
+    }
+
+    public static boolean isEnabled() {
+        return Utils.readFile(TOGGLE).equals("1");
+    }
+
+    public static boolean hasToggle() {
+        return Utils.existFile(TOGGLE);
+    }
+
+    public static boolean supported() {
+        return Utils.existFile(PARENT);
+    }
+
+    private static void run(String command, String id, Context context) {
+        Control.runSetting(command, ApplyOnBootFragment.CPU_HOTPLUG, id, context);
+    }
+
+}