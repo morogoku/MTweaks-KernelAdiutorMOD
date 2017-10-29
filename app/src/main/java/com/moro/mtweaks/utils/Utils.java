dcb332c7dbded5f192dec0a884167daa1bef3c13
morogoku <morogoku@hotmail.com>
2017-10-29 18:58:57 +0100

    fix activity-alias error with gradle 3.0.0
    Signed-off-by: morogoku <morogoku@hotmail.com>

--- app/src/main/java/com/moro/mtweaks/utils/Utils.java
+++ app/src/main/java/com/moro/mtweaks/utils/Utils.java
@@ -144,7 +144,7 @@ public class Utils {
                 material ? PackageManager.COMPONENT_ENABLED_STATE_DISABLED :
                         PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
         pm.setComponentEnabledSetting(new ComponentName(BuildConfig.APPLICATION_ID,
-                        BuildConfig.APPLICATION_ID + ".activities.StartActivity-Material"),
+                        BuildConfig.APPLICATION_ID + ".activities.StartActivity.Material"),
                 material ? PackageManager.COMPONENT_ENABLED_STATE_ENABLED :
                         PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
     }
