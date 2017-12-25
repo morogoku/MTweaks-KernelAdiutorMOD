SmartPack/SmartPack-Kernel-Manager

Use foreground for services

app/src/main/java/com/grarak/kerneladiutor/fragments/other/SettingsFragment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2016 Willi Ye <williye97@gmail.com>
+ * Copyright (C) 2015-2017 Willi Ye <williye97@gmail.com>
  *
  * This file is part of Kernel Adiutor.
  *
@@ -50,7 +50,7 @@ import com.grarak.kerneladiutor.R;
 import com.grarak.kerneladiutor.activities.BannerResizerActivity;
 import com.grarak.kerneladiutor.activities.MainActivity;
 import com.grarak.kerneladiutor.activities.NavigationActivity;
-import com.grarak.kerneladiutor.services.boot.Service;
+import com.grarak.kerneladiutor.services.boot.ApplyOnBootService;
 import com.grarak.kerneladiutor.utils.Prefs;
 import com.grarak.kerneladiutor.utils.Utils;
 import com.grarak.kerneladiutor.utils.ViewUtils;
@@ -293,12 +293,12 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
                 }
                 return true;
             case KEY_APPLY_ON_BOOT_TEST:
-                if (Utils.isServiceRunning(Service.class, getActivity())) {
+                if (Utils.isServiceRunning(ApplyOnBootService.class, getActivity())) {
                     Utils.toast(R.string.apply_on_boot_running, getActivity());
                 } else {
-                    Intent intent = new Intent(getActivity(), Service.class);
+                    Intent intent = new Intent(getActivity(), ApplyOnBootService.class);
                     intent.putExtra("messenger", new Messenger(new MessengerHandler(getActivity())));
-                    getActivity().startService(intent);
+                    Utils.startService(getActivity(), intent);
                 }
                 return true;
             case KEY_LOGCAT:

app/src/main/java/com/grarak/kerneladiutor/services/boot/ApplyOnBootService.java
@@ -19,7 +19,13 @@
  */
 package com.grarak.kerneladiutor.services.boot;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Message;
@@ -27,17 +33,41 @@ import android.os.Messenger;
 import android.os.RemoteException;
 import android.support.annotation.Nullable;
 
+import com.grarak.kerneladiutor.R;
 import com.grarak.kerneladiutor.utils.Utils;
 
 /**
  * Created by willi on 03.05.16.
  */
-public class Service extends android.app.Service {
+public class ApplyOnBootService extends Service {
+
+    private static final int SERVICE_FOREGROUND_ID = 1;
+    static final String CHANNEL_ID = "onboot_notification_channel";
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,
+                    getString(R.string.apply_on_boot), NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setSound(null, null);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+            Notification.Builder builder = new Notification.Builder(
+                    this, CHANNEL_ID);
+            builder.setContentTitle(getString(R.string.apply_on_boot))
+                    .setSmallIcon(R.mipmap.ic_launcher);
+            startForeground(SERVICE_FOREGROUND_ID, builder.build());
+        }
     }
 
     @Override
@@ -72,7 +102,6 @@ public class Service extends android.app.Service {
             }
             stopSelf();
         }
-
         return START_NOT_STICKY;
     }
 

app/src/main/java/com/grarak/kerneladiutor/services/boot/OnBootReceiver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2016 Willi Ye <williye97@gmail.com>
+ * Copyright (C) 2015-2017 Willi Ye <williye97@gmail.com>
  *
  * This file is part of Kernel Adiutor.
  *
@@ -22,28 +22,29 @@ package com.grarak.kerneladiutor.services.boot;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 
 import com.grarak.kerneladiutor.services.monitor.Monitor;
 import com.grarak.kerneladiutor.utils.Prefs;
+import com.grarak.kerneladiutor.utils.Utils;
+import com.grarak.kerneladiutor.utils.root.RootUtils;
 
 /**
  * Created by willi on 03.05.16.
  */
-public class Receiver extends BroadcastReceiver {
+public class OnBootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                JobService.start(context);
-            } else {
-                context.startService(new Intent(context, Service.class));
+            RootUtils.SU su = new RootUtils.SU();
+            su.runCommand("echo /testRoot/");
+            if (!su.denied) {
+                Utils.startService(context, new Intent(context, ApplyOnBootService.class));
+                if (Prefs.getBoolean("data_sharing", true, context)) {
+                    Utils.startService(context, new Intent(context, Monitor.class));
+                }
             }
-            if (Prefs.getBoolean("data_sharing", true, context)) {
-                context.startService(new Intent(context, Monitor.class));
-            }
+            su.close();
         }
     }
-
 }

app/src/main/java/com/grarak/kerneladiutor/services/monitor/Monitor.java
@@ -19,18 +19,27 @@
  */
 package com.grarak.kerneladiutor.services.monitor;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.grarak.kerneladiutor.BuildConfig;
+import com.grarak.kerneladiutor.R;
+import com.grarak.kerneladiutor.activities.MainActivity;
 import com.grarak.kerneladiutor.database.Settings;
+import com.grarak.kerneladiutor.fragments.tools.DataSharingFragment;
 import com.grarak.kerneladiutor.utils.Device;
 import com.grarak.kerneladiutor.utils.Prefs;
 import com.grarak.kerneladiutor.utils.Utils;
@@ -49,6 +58,9 @@ import java.util.concurrent.TimeUnit;
  */
 
 public class Monitor extends Service {
+
+    private static final String CHANNEL_ID = "monitor_notification_channel";
+    private static final int SERVICE_FOREGROUND_ID = 2;
 
     private int mLevel;
     private long mTime;
@@ -97,6 +109,7 @@ public class Monitor extends Service {
     private BroadcastReceiver mScreenReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
+            Log.i("blaa", "screen");
             mScreenOn = intent.getAction().equals(Intent.ACTION_SCREEN_ON);
             if (!mScreenOn && !mCalculating) {
                 mLevel = 0;
@@ -171,8 +184,39 @@ public class Monitor extends Service {
     }
 
     @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
     public void onCreate() {
         super.onCreate();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,
+                    getString(R.string.data_sharing), NotificationManager.IMPORTANCE_MIN);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+            PendingIntent disableIntent = PendingIntent.getBroadcast(this, 1,
+                    new Intent(this, DisableReceiver.class),
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+
+            Intent launchIntent = new Intent(this, MainActivity.class);
+            launchIntent.putExtra("section", DataSharingFragment.class.getCanonicalName());
+            PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                    launchIntent, 0);
+
+            Notification.Builder builder =
+                    new Notification.Builder(this, CHANNEL_ID);
+            builder.setContentTitle(getString(R.string.data_sharing))
+                    .setContentText(getString(R.string.data_sharing_summary_notification))
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentIntent(contentIntent)
+                    .addAction(0, getString(R.string.disable), disableIntent);
+            startForeground(SERVICE_FOREGROUND_ID, builder.build());
+        }
 
         registerReceiver(mBatteryReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
 
@@ -185,15 +229,38 @@ public class Monitor extends Service {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        return START_STICKY;
-    }
-
-    @Override
     public void onDestroy() {
         super.onDestroy();
         unregisterReceiver(mBatteryReceiver);
         unregisterReceiver(mScreenReceiver);
     }
+
+    public static class DisableService extends Service {
+
+        @Nullable
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+
+        @Override
+        public void onCreate() {
+            super.onCreate();
+            Prefs.saveBoolean("data_sharing", false, this);
+            stopSelf();
+        }
+
+    }
+
+    public static class DisableReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            context.startService(new Intent(context, DisableService.class));
+            context.stopService(new Intent(context, Monitor.class));
+        }
+
+    }
+
 
 }

morogoku/MTweaks-KernelAdiutorMOD

app/src/main/java/com/moro/mtweaks/fragments/other/SettingsFragment.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2016 Willi Ye <williye97@gmail.com>
+ * Copyright (C) 2015-2017 Willi Ye <williye97@gmail.com>
  *
  * This file is part of Kernel Adiutor.
  *
@@ -51,7 +51,7 @@ import com.moro.mtweaks.R;
 import com.moro.mtweaks.activities.BannerResizerActivity;
 import com.moro.mtweaks.activities.MainActivity;
 import com.moro.mtweaks.activities.NavigationActivity;
-import com.moro.mtweaks.services.boot.Service;
+import com.moro.mtweaks.services.boot.ApplyOnBootService;
 import com.moro.mtweaks.utils.Prefs;
 import com.moro.mtweaks.utils.Utils;
 import com.moro.mtweaks.utils.ViewUtils;
@@ -301,12 +301,12 @@ public class SettingsFragment extends PreferenceFragmentCompat implements
                 }
                 return true;
             case KEY_APPLY_ON_BOOT_TEST:
-                if (Utils.isServiceRunning(Service.class, getActivity())) {
+                if (Utils.isServiceRunning(ApplyOnBootService.class, getActivity())) {
                     Utils.toast(R.string.apply_on_boot_running, getActivity());
                 } else {
-                    Intent intent = new Intent(getActivity(), Service.class);
+                    Intent intent = new Intent(getActivity(), ApplyOnBootService.class);
                     intent.putExtra("messenger", new Messenger(new MessengerHandler(getActivity())));
-                    getActivity().startService(intent);
+                    Utils.startService(getActivity(), intent);
                 }
                 return true;
             case KEY_LOGCAT:

app/src/main/java/com/moro/mtweaks/services/boot/ApplyOnBootService.java
@@ -19,7 +19,13 @@
  */
 package com.moro.mtweaks.services.boot;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.Message;
@@ -27,12 +33,16 @@ import android.os.Messenger;
 import android.os.RemoteException;
 import android.support.annotation.Nullable;
 
+import com.moro.mtweaks.R;
 import com.moro.mtweaks.utils.Utils;
 
 /**
  * Created by willi on 03.05.16.
  */
-public class Service extends android.app.Service {
+public class ApplyOnBootService extends Service {
+
+    private static final int SERVICE_FOREGROUND_ID = 1;
+    static final String CHANNEL_ID = "onboot_notification_channel";
 
     @Nullable
     @Override
@@ -41,8 +51,27 @@ public class Service extends android.app.Service {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public void onCreate() {
+        super.onCreate();
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,
+                    getString(R.string.apply_on_boot), NotificationManager.IMPORTANCE_DEFAULT);
+            notificationChannel.setSound(null, null);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+            Notification.Builder builder = new Notification.Builder(
+                    this, CHANNEL_ID);
+            builder.setContentTitle(getString(R.string.apply_on_boot))
+                    .setSmallIcon(R.mipmap.ic_launcher);
+            startForeground(SERVICE_FOREGROUND_ID, builder.build());
+        }
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
         Messenger messenger = null;
         if (intent != null) {
             Bundle extras = intent.getExtras();
@@ -73,9 +102,7 @@ public class Service extends android.app.Service {
             }
             stopSelf();
         }
-
         return START_NOT_STICKY;
-
     }
 
 }

app/src/main/java/com/moro/mtweaks/services/boot/OnBootReceiver.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2016 Willi Ye <williye97@gmail.com>
+ * Copyright (C) 2015-2017 Willi Ye <williye97@gmail.com>
  *
  * This file is part of Kernel Adiutor.
  *
@@ -22,29 +22,29 @@ package com.moro.mtweaks.services.boot;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 
 import com.moro.mtweaks.services.monitor.Monitor;
 import com.moro.mtweaks.utils.Prefs;
+import com.moro.mtweaks.utils.Utils;
+import com.moro.mtweaks.utils.root.RootUtils;
 
 /**
  * Created by willi on 03.05.16.
  */
-public class Receiver extends BroadcastReceiver {
+public class OnBootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                JobService.start(context);
-            } else {
-                context.startService(new Intent(context, Service.class));
+            RootUtils.SU su = new RootUtils.SU();
+            su.runCommand("echo /testRoot/");
+            if (!su.denied) {
+                Utils.startService(context, new Intent(context, ApplyOnBootService.class));
+                if (Prefs.getBoolean("data_sharing", true, context)) {
+                    Utils.startService(context, new Intent(context, Monitor.class));
+                }
             }
-            if (Prefs.getBoolean("data_sharing", true, context)) {
-                context.startService(new Intent(context, Monitor.class));
-            }
-            Prefs.saveBoolean("is_booted", true, context);
+            su.close();
         }
     }
-
 }

app/src/main/java/com/moro/mtweaks/services/monitor/Monitor.java
@@ -19,18 +19,27 @@
  */
 package com.moro.mtweaks.services.monitor;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.support.annotation.Nullable;
+import android.util.Log;
 
 import com.moro.mtweaks.BuildConfig;
+import com.moro.mtweaks.R;
+import com.moro.mtweaks.activities.MainActivity;
 import com.moro.mtweaks.database.Settings;
+import com.moro.mtweaks.fragments.tools.DataSharingFragment;
 import com.moro.mtweaks.utils.Device;
 import com.moro.mtweaks.utils.Prefs;
 import com.moro.mtweaks.utils.Utils;
@@ -51,6 +60,9 @@ import mtweaks.services.monitor.IMonitor;
  */
 
 public class Monitor extends Service {
+
+    private static final String CHANNEL_ID = "monitor_notification_channel";
+    private static final int SERVICE_FOREGROUND_ID = 2;
 
     private int mLevel;
     private long mTime;
@@ -99,6 +111,7 @@ public class Monitor extends Service {
     private BroadcastReceiver mScreenReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
+            Log.i("blaa", "screen");
             mScreenOn = intent.getAction().equals(Intent.ACTION_SCREEN_ON);
             if (!mScreenOn && !mCalculating) {
                 mLevel = 0;
@@ -173,8 +186,39 @@ public class Monitor extends Service {
     }
 
     @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
+
+    @Override
     public void onCreate() {
         super.onCreate();
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationManager notificationManager =
+                    (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID,
+                    getString(R.string.data_sharing), NotificationManager.IMPORTANCE_MIN);
+            notificationManager.createNotificationChannel(notificationChannel);
+
+            PendingIntent disableIntent = PendingIntent.getBroadcast(this, 1,
+                    new Intent(this, DisableReceiver.class),
+                    PendingIntent.FLAG_UPDATE_CURRENT);
+
+            Intent launchIntent = new Intent(this, MainActivity.class);
+            launchIntent.putExtra("section", DataSharingFragment.class.getCanonicalName());
+            PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                    launchIntent, 0);
+
+            Notification.Builder builder =
+                    new Notification.Builder(this, CHANNEL_ID);
+            builder.setContentTitle(getString(R.string.data_sharing))
+                    .setContentText(getString(R.string.data_sharing_summary_notification))
+                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setContentIntent(contentIntent)
+                    .addAction(0, getString(R.string.disable), disableIntent);
+            startForeground(SERVICE_FOREGROUND_ID, builder.build());
+        }
 
         registerReceiver(mBatteryReceiver, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
 
@@ -187,15 +231,38 @@ public class Monitor extends Service {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        return START_STICKY;
-    }
-
-    @Override
     public void onDestroy() {
         super.onDestroy();
         unregisterReceiver(mBatteryReceiver);
         unregisterReceiver(mScreenReceiver);
     }
+
+    public static class DisableService extends Service {
+
+        @Nullable
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
+
+        @Override
+        public void onCreate() {
+            super.onCreate();
+            Prefs.saveBoolean("data_sharing", false, this);
+            stopSelf();
+        }
+
+    }
+
+    public static class DisableReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            context.startService(new Intent(context, DisableService.class));
+            context.stopService(new Intent(context, Monitor.class));
+        }
+
+    }
+
 
 }